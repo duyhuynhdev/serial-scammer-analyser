9ac61f023625201f4b321215e591ab78af328ad8
Dustin <duyhuynhdev@gmail.com>
2024-09-16 15:55:16 +1000

    Big Update

--- main/algorithms/PatternExplorer.py
+++ main/algorithms/PatternExplorer.py
@@ -1,17 +1,35 @@
+from algorithms.ScammerNetworkBuilder import dataloader
 from entity.Cluster import Cluster
 from entity import Node
 from utils.DataLoader import DataLoader
 
+dataloader = DataLoader()
+
 
 def execute(scammer_address):
-    node = Node.create_node(scammer_address, None, DataLoader())
+    node = Node.create_node(scammer_address, None, dataloader)
+    scammer_address = "aaaa"
+    is_scammer = scammer_address in dataloader.scammers
+    end_nodes = (dataloader.bridge_addresses |
+                 dataloader.defi_addresses |
+                 dataloader.MEV_addresses |
+                 dataloader.mixer_addresses |
+                 dataloader.wallet_addresses |
+                 dataloader.other_addresses)
+    start_address = ""
+
+    # while start_address not in end_nodes:
+    #     start_address =  next_address()
+
     print(len(node.normal_txs))
     print(len(node.internal_txs))
     pass
+
 
 def chain_pattern_detection(cluster: Cluster):
     pass
 
 
 if __name__ == '__main__':
-    execute("0x48f0fc8dfc672dd45e53b6c53cd5b09c71d9fbd6")
+    # execute("0x48f0fc8dfc672dd45e53b6c53cd5b09c71d9fbd6")
+    print(dataloader.scammers)
--- main/entity/blockchain/Transaction.py
+++ main/entity/blockchain/Transaction.py
@@ -1,5 +1,6 @@
 from entity.blockchain.DTO import DTO
 
+
 class Transaction(DTO):
     def __init__(self, blockNumber=None, timeStamp=None, hash=None, sender=None, to=None, value=None, gas=None, gasUsed=None, contractAddress=None, input=None, isError=None):
         super().__init__()
@@ -19,15 +20,22 @@ class Transaction(DTO):
         for name, value in dict.items():
             setattr(self, name, value)
 
+    def get_transaction_amount(self):
+        return int(self.value) / 10 ** 18
+
+
 
 class NormalTransaction(Transaction):
-    def __init__(self, blockNumber=None, timeStamp=None, hash=None, sender=None, to=None, value=None, gas=None, gasUsed=None, contractAddress=None, input=None, isError=None, gasPrice=None, methodId=None, functionName=None, cumulativeGasUsed=None):
+    def __init__(self, blockNumber=None, timeStamp=None, hash=None, sender=None, to=None, value=None, gas=None, gasUsed=None, contractAddress=None, input=None, isError=None, gasPrice=None,
+                 methodId=None, functionName=None, cumulativeGasUsed=None):
         super().__init__(blockNumber, timeStamp, hash, sender, to, value, gas, gasUsed, contractAddress, input, isError)
         self.functionName = functionName
         self.methodId = methodId
         self.gasPrice = gasPrice
         self.cumulativeGasUsed = cumulativeGasUsed
 
+    def get_transaction_fee(self):
+        return int(self.gasPrice * self.gasUsed) / 10 ** 18
 
 class InternalTransaction(Transaction):
     def __init__(self, blockNumber=None, timeStamp=None, hash=None, sender=None, to=None, value=None, gas=None, gasUsed=None, contractAddress=None, input=None, isError=None, type=None, errCode=None):
@@ -35,8 +43,10 @@ class InternalTransaction(Transaction):
         self.type = type
         self.errCode = errCode
 
+
 class SwapTransaction(NormalTransaction):
-    def __init__(self, blockNumber=None, timeStamp=None, hash=None, sender=None, to=None, value=None, gas=None, gasUsed=None, contractAddress=None, input=None, isError=None, gasPrice=None, methodId=None, functionName=None, cumulativeGasUsed=None):
+    def __init__(self, blockNumber=None, timeStamp=None, hash=None, sender=None, to=None, value=None, gas=None, gasUsed=None, contractAddress=None, input=None, isError=None, gasPrice=None,
+                 methodId=None, functionName=None, cumulativeGasUsed=None):
         super().__init__(blockNumber, timeStamp, hash, sender, to, value, gas, gasUsed, contractAddress, input, isError, gasPrice, methodId, functionName, cumulativeGasUsed)
         self.token0 = None,
         self.token1 = None,
