38edc8237714c40875a750f6273c4c7c0094fa60
hooniez <myeonghoon.sun.coder@gmail.com>
2024-09-25 12:52:45 +1000

    feat: add a command to pull data from the s3 bucket to the local storage as part of sync()

--- main/utils/S3Syncer.py
+++ main/utils/S3Syncer.py
@@ -1,10 +1,12 @@
 from functools import cached_property
+from typing import List
+
 import boto3
 from pathlib import Path
 import subprocess
 
 
-class S3FileManager:
+class S3Syncer:
     def __init__(
         self, bucket_name="serial-scammer-analyser-bucket", data_dir="resources/data"
     ):
@@ -20,7 +22,7 @@ class S3FileManager:
     @cached_property
     def project_root(self):
         for parent in self.current_file_path.parents:
-            if (parent / ".git").exists():
+            if (parent / "requirements.txt").exists():
                 return parent
         # If no .git is found, raise an exception
         raise FileNotFoundError("No project root directory containing '.git' found.")
@@ -34,24 +36,31 @@ class S3FileManager:
         Call this function to synchronise the s3 bucket with the items in data_directory
         """
         local_path = str(self.data_directory)
-        command = ["aws", "s3", "sync", local_path, f"s3://{self.bucket_name}"]
+        command_to_sync_local_to_bucket = ["aws", "s3", "sync", local_path, f"s3://{self.bucket_name}"]
+        command_to_sync_bucket_to_local = ["aws", "s3", "sync", f"s3://{self.bucket_name}", local_path]
 
-        with subprocess.Popen(
-            command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
-        ) as process:
-            for line in process.stdout:
-                print(line, end="")  # Print each line as it comes in
+        sync_commands = [command_to_sync_local_to_bucket, command_to_sync_bucket_to_local]
+        for sync_command in sync_commands:
+            self.run_command(sync_command)
 
-            stderr_output = process.stderr.read()  # Capture any error output
-            if stderr_output:
-                print(stderr_output)
-
+    def run_command(self, command: List[str]) -> None:
+        """
+        Runs a system command using subprocess and handles output/error.
+        """
         try:
-            process.wait()  # Wait for the process to finish
-            if process.returncode != 0:
-                print(f"Error syncing files: {process.returncode}")
+            with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) as process:
+                for line in process.stdout:
+                    print(line, end="")  # Print each line as it comes in
+
+                stderr_output = process.stderr.read()  # Capture any error output
+                if stderr_output:
+                    print(f"Error output: {stderr_output}")
+
+                process.wait()  # Wait for the process to finish
+                if process.returncode != 0:
+                    print(f"Error syncing files: {process.returncode}")
         except Exception as e:
-            print(f"Error syncing files: {e}")
+            print(f"Failed to execute command {' '.join(command)}: {e}")
 
 
 if __name__ == "__main__":
@@ -63,12 +72,14 @@ if __name__ == "__main__":
 
        - `data_dir` is the path to the local directory that will be synchronized with the corresponding directory in your S3 bucket.
 
-    2. To synchronize files between the S3 bucket and the local directory, simply call the `sync()` method wherever necessary in your program:
+    2. To synchronize files between the S3 bucket and the local directory, simply call the `sync()` method wherever
+       necessary in your program:
 
        s3_file_manager.sync()
 
        - This will ensure that any new or modified files in either the local directory or the S3 bucket are synchronized.
     """
     # Example usage:
-    s3_file_manager = S3FileManager(data_dir="resources/data/uniswap")
+
+    s3_file_manager = S3Syncer(data_dir="resources/data")
     s3_file_manager.sync()
