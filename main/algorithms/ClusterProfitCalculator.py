ee0089c97be6068b3ea076575d5183fdc5d55f29
hooniez <myeonghoon.sun.coder@gmail.com>
2024-09-27 17:26:41 +1000

    feat: refactor `calculate_profit_per_pool` for modularity and update logic
    - Modularised `calculate_profit_per_pool` by breaking it into smaller, more focused functions.
    - Updated the logic for calculating `y` and `z`:
      - `y` now includes the total sell amounts for all swaps where the `swap_to` address belongs to a node in the cluster.
      - `z` now includes the total buy amounts for all swaps where the `swap_from` address belongs to a node in the cluster.

--- main/algorithms/ClusterProfitCalculator.py
+++ main/algorithms/ClusterProfitCalculator.py
@@ -1,10 +1,9 @@
 from functools import cached_property
 
-from entity.Cluster import ClusterNode, Cluster
+from entity.Cluster import ClusterNode
 from entity.Node import NodeLabel
-from entity.blockchain.Address import Pool
+from entity.blockchain.Address import Pool, SwapDirection
 from typing import Set, List
-
 from utils import DataLoader
 
 
@@ -14,16 +13,23 @@ class ClusterProfitCalculator:
         self.cluster = None  # self.cluster will be set in self.calculate()
 
     @cached_property
-    def scammer_nodes(self) -> Set[ClusterNode]:
+    def node_addresses_in_cluster(self) -> Set[str]:
+        return {node.address.lower() for node in self.cluster}
+
+    @cached_property
+    def scammer_node_addresses(self) -> Set[str]:
         """
         Return a set of scammer nodes in the cluster
         """
         return {
-            node
+            node.address.lower()
             for node in self.cluster
             if (
                 NodeLabel.is_scammer(node)
-                and node.address in self.dataloader.unique_scammers
+                # TODO: remove this line once we're confident that the given cluster is made only of scammer nodes
+                and node.address
+                in self.dataloader.scammers_set
+
             )
         }
 
@@ -34,80 +40,70 @@ class ClusterProfitCalculator:
         """
         return {
             pool
-            for scammer_node in self.scammer_nodes
-            for pool in DataLoader.load_pool(scammer_node.address, self.dataloader)
+            for scammer_node_address in self.scammer_node_addresses
+            for pool in DataLoader.load_pool(scammer_node_address, self.dataloader)
         }
 
-    @cached_property
-    def washer_nodes(self) -> Set[ClusterNode]:
-        """
-        Return a set of washer nodes in the cluster
-        """
-        swap_tos = {
-            swap.to.lower() for pool in self.scammer_pools for swap in pool.swaps
-        }
-        return {node for node in self.cluster if node.address in swap_tos}
+    def initialise_cluster(self, cluster: List[ClusterNode]) -> None:
+        self.cluster = cluster
+        # Invalidate cached properties for a new cluster
+        self.__dict__.pop("node_addresses_in_cluster", None)
+        self.__dict__.pop("scammer_node_addresses", None)
+        self.__dict__.pop("scammer_pools", None)
 
     def calculate(self, cluster: List[ClusterNode]) -> float:
         """
         Reuse the same instance for calculating profits on different clusters.
         """
-        self.cluster = cluster
-        # Invalidate cached properties for a new cluster
-        self.__dict__.pop("scammer_nodes", None)
-        self.__dict__.pop("scammer_pools", None)
-        self.__dict__.pop("washer_nodes", None)
+        self.initialise_cluster(cluster)
 
         return sum(
-            self.calculate_per_pool(scammer_pool) for scammer_pool in self.scammer_pools
+            self.calculate_profit_per_pool(scammer_pool) for scammer_pool in self.scammer_pools
         )
 
-    def calculate_per_pool(self, pool: Pool) -> float:
-        """
-        Calculate the true profit made in a given pool
-        """
-        high_value_token_position = pool.get_high_value_position()
-        scam_token_position = 1 - high_value_token_position
+    def calculate_y_per_pool(self, pool: Pool) -> float:
+        y = 0.0
 
-        y = 0
-        match pool.burns:
-            # If the number of burn events is 1, the type of rug-pull is simple
-            case [_]:  # Wildcard to match any single item without binding to a variable
-                burn_total, fee_total = pool.get_total_burn_value(
-                    high_value_token_position
-                )
-                y += burn_total - fee_total
-            # If the number of burn events is greater than 1, the type of rug-pull is "sell"
-            case [_, *_]:
-                max_swap, swap_fee = pool.get_max_swap_value(high_value_token_position)
-                y += max_swap - swap_fee
-            case _:
-                raise ValueError("pool.burns should have at least one burn")
+        burn_total, fee_total = pool.get_total_burn_value()
+        y += burn_total - fee_total
 
-        x = 0
-        mint_total, fee_total = pool.get_total_mint_value(high_value_token_position)
-        scam_token = pool.get_scam_token(scam_token_position)
+        sell_total, fee_total = pool.get_total_swap_value(self.node_addresses_in_cluster, SwapDirection.OUT)
+        y += sell_total - fee_total
+
+        return y
+
+    def calculate_x_per_pool(self, pool: Pool) -> float:
+        x = 0.0
+
+        mint_total, fee_total = pool.get_total_mint_value()
+        scam_token = pool.get_scam_token()
         normal_txs, _ = DataLoader.load_transaction_by_address(scam_token.creator)
-        creation_fee = next(
+        token_creation_fee = next(
             (
                 normal_tx.get_transaction_fee()
                 for normal_tx in normal_txs
                 if normal_tx.hash == scam_token.creation_tx
             ),
+            # TODO: add a check to return an error if creation_fee is 0.
             0,  # Default value if no matching transaction is found
         )
-        x += mint_total + fee_total + creation_fee
+        x += mint_total + fee_total + token_creation_fee
+        return x
 
-        z = 0
-        for washer_node in self.washer_nodes:
-            buy_amount, fee_total_in = pool.get_swap_in_value(
-                high_value_token_position, washer_node.address
-            )
+    def calculate_z_per_pool(self, pool: Pool) -> float:
+        z = 0.0
 
-            sell_amount, fee_total_out = pool.get_swap_out_value(
-                high_value_token_position, washer_node.address
-            )
-            z += (buy_amount + fee_total_in) - (sell_amount - fee_total_out)
+        buy_total, fee_total = pool.get_total_swap_value(self.node_addresses_in_cluster, SwapDirection.IN)
+        z += buy_total + fee_total
+        return z
+
+    def calculate_profit_per_pool(self, pool: Pool) -> float:
+        """
+        Calculate the true profit made in a given pool
+        """
+        y = self.calculate_y_per_pool(pool)
+        x = self.calculate_x_per_pool(pool)
+        z = self.calculate_z_per_pool(pool)
 
         return y - x - z
 
@@ -116,6 +112,7 @@ class ClusterProfitCalculator:
         Batch process multiple clusters by resetting cached properties between calculations.
         """
         return [self.calculate(cluster) for cluster in clusters]
+
 
 
 if __name__ == "__main__":
