3b9a1aa6e526974cb120d961c43a96a82a1bda1c
Cervenjak <joshcervenjak@gmail.com>
2024-10-05 18:10:08 +1000

    Uplifting simple chain logic

--- main/algorithms/SimpleChainFinder.py
+++ main/algorithms/SimpleChainFinder.py
@@ -1,4 +1,8 @@
+import ast
+import csv
 import os
+
+import numpy as np
 
 from algorithms.ScammerNetworkBuilder import dataloader
 from data_collection.AccountCollector import TransactionCollector
@@ -13,14 +17,13 @@ REMOVE_LIQUIDITY_SUBSTRING = "removeLiquidity"
 ADD_LIQUIDITY_SUBSTRING = "addLiquidity"
 
 
-
 def chain_pattern_detection(starter_address):
     """Function that returns a single chain list scam from a scammer address. Includes the entire chain the address
     is involved in"""
     fwd_chain = []
     valid_address_fwd = valid_address_bwd = starter_address in dataloader.scammers
     if valid_address_fwd:
-        fwd_chain.append(starter_address)
+        fwd_chain.append([starter_address])
 
     starter_transaction_history = transaction_collector.get_transactions(starter_address)
     current_transaction_history = starter_transaction_history
@@ -29,16 +32,23 @@ def chain_pattern_detection(starter_address):
     # chain forward
     while valid_address_fwd:
         valid_address_fwd = False
-        largest_out_transaction = get_largest_out_after_remove_liquidity(current_address, current_transaction_history)[0]
+        largest_out_transaction, _, num_remove_calls = get_largest_out_after_remove_liquidity(current_address, current_transaction_history)
         if largest_out_transaction and largest_out_transaction.to in dataloader.scammers:
-            largest_in_transaction, next_transaction_history = get_largest_in_before_add_liquidity(largest_out_transaction.to)
+            largest_in_transaction, next_transaction_history, _ = get_largest_in_before_add_liquidity(largest_out_transaction.to)
             if largest_in_transaction:
                 valid_address_fwd = largest_out_transaction.hash == largest_in_transaction.hash
 
         if valid_address_fwd:
+            # we now have data of the current node, append that
+            fwd_chain[-1].extend([num_remove_calls, largest_out_transaction.timeStamp, largest_out_transaction.get_transaction_amount()])
             current_address = largest_in_transaction.to
             current_transaction_history = next_transaction_history
-            fwd_chain.append(current_address)
+            fwd_chain.append([current_address])
+
+        # since the chain has ended, we may not correctly return num_remove_calls
+        # so just manually look
+        num_remove_calls = count_number_of_remove_liquidity_calls(current_address, current_transaction_history)
+        fwd_chain[-1].append(num_remove_calls)
 
     current_address = starter_address
     current_transaction_history = starter_transaction_history
@@ -47,18 +57,28 @@ def chain_pattern_detection(starter_address):
         valid_address_bwd = False
         largest_in_transaction = get_largest_in_before_add_liquidity(current_address, current_transaction_history)[0]
         if largest_in_transaction and largest_in_transaction.sender in dataloader.scammers:
-            largest_out_transaction, prev_transaction_history = get_largest_out_after_remove_liquidity(largest_in_transaction.sender)
+            largest_out_transaction, prev_transaction_history, num_remove_calls = get_largest_out_after_remove_liquidity(largest_in_transaction.sender)
             if largest_out_transaction:
                 valid_address_bwd = largest_out_transaction.hash == largest_in_transaction.hash
 
         if valid_address_bwd:
             current_address = largest_out_transaction.sender
             current_transaction_history = prev_transaction_history
-            bwd_chain.append(current_address)
+            bwd_chain.append([current_address, num_remove_calls, largest_in_transaction.timeStamp, largest_in_transaction.get_transaction_amount()])
 
     bwd_chain = bwd_chain[::-1]
     return bwd_chain + fwd_chain
 
+def count_number_of_remove_liquidity_calls(scammer_address, normal_txs=None):
+    num_remove_liquidity_calls = 0
+    if normal_txs is None:
+        normal_txs = transaction_collector.get_transactions(scammer_address)
+
+    for transaction in normal_txs:
+        if REMOVE_LIQUIDITY_SUBSTRING in str(transaction.functionName) and not transaction.isError:
+            num_remove_liquidity_calls += 1
+
+    return num_remove_liquidity_calls
 
 def get_largest_out_after_remove_liquidity(scammer_address: str, normal_txs=None):
     if normal_txs is None:
@@ -73,23 +93,26 @@ def get_largest_in_before_add_liquidity(scammer_address: str, normal_txs=None):
 
 
 def get_largest_transaction(normal_txs, scammer_address, liquidity_function_name: str, is_out, *range_loop_args):
+    num_remove_liquidities_found = 0
     passed_liquidity_function = False
     exists_duplicate_amount = False
     largest_transaction = None
     for index in range(range_loop_args[0], range_loop_args[1], range_loop_args[2]):
-        if not passed_liquidity_function and liquidity_function_name in str(normal_txs[index].functionName) and not normal_txs[index].isError:
+        if liquidity_function_name in str(normal_txs[index].functionName) and not normal_txs[index].isError:
             passed_liquidity_function = True
+            # count the number of remove liquidity functions
+            if liquidity_function_name == REMOVE_LIQUIDITY_SUBSTRING:
+                num_remove_liquidities_found += 1
             if largest_transaction is None:
-                return None, normal_txs
+                return None, normal_txs, num_remove_liquidities_found
         elif (is_out and normal_txs[index].is_to_eoa(scammer_address)) or (not is_out and normal_txs[index].is_in_tx(scammer_address)):
-
             # just set the largest_transaction for the first find
             if largest_transaction is None:
                 largest_transaction = normal_txs[index]
             elif normal_txs[index].get_transaction_amount() >= largest_transaction.get_transaction_amount():
                 # >= amount found then current largest, therefore is not the sole funder
                 if passed_liquidity_function:
-                    return None, normal_txs
+                    return None, normal_txs, num_remove_liquidities_found
 
                 # if this new transaction is the same amount already, indicate that there is a duplicate
                 if normal_txs[index].get_transaction_amount() == largest_transaction.get_transaction_amount():
@@ -98,7 +121,7 @@ def get_largest_transaction(normal_txs, scammer_address, liquidity_function_name
                     exists_duplicate_amount = False
                     largest_transaction = normal_txs[index]
 
-    return largest_transaction if not exists_duplicate_amount and passed_liquidity_function else None, normal_txs
+    return largest_transaction if not exists_duplicate_amount and passed_liquidity_function else None, normal_txs, num_remove_liquidities_found
 
 
 def read_from_csv_as_set(input_path):
@@ -115,7 +138,7 @@ def read_from_csv_as_set(input_path):
 
 
 def run_chain_on_scammers():
-    scammer_chain_path = os.path.join(path.univ2_scammer_chain_path, "simple_chain.txt")
+    scammer_chain_path = os.path.join(path.univ2_scammer_chain_path, "simple_chain.csv")
 
     existing_addresses = read_from_csv_as_set(scammer_chain_path)
     scammers_remaining = set(dataloader.scammers)
@@ -142,6 +165,31 @@ def run_chain_on_scammers():
 
 
 if __name__ == '__main__':
-    run_chain_on_scammers()
+    # run_chain_on_scammers()
     # print(*chain_pattern_detection("0x48f0fc8dfc672dd45e53b6c53cd5b09c71d9fbd6"), sep='\n')
-    # print(chain_pattern_detection("0x48f0fc8dfc672dd45e53b6c53cd5b09c71d9fbd6"))
+    print(chain_pattern_detection("0x48f0fc8dfc672dd45e53b6c53cd5b09c71d9fbd6"))
+
+    # REFERENCE later
+    # field_names = ["length", "scammer_chain"]
+    # data = [
+    #     [15, [(3, "scammer_1", "some_amount"), (5, "scammer_2", "another_amount")]]
+    # ]
+
+    scammer_chain_path = os.path.join(path.univ2_scammer_chain_path, "simple_chain.csv")
+    # REFERENCE later for how to read write to file using appened "a"
+    # with open(scammer_chain_path, "a", newline='') as csv_file:
+    #     writer = csv.writer(csv_file, quotechar='"', delimiter='|', quoting=csv.QUOTE_ALL)
+    #     writer.writerows(data)
+
+    # REFERENCE later for how to write to file
+    # my_result = []
+    # with open(scammer_chain_path, "r") as csv_file:
+    #     reader = csv.reader(csv_file, quotechar='"', delimiter='|', quoting=csv.QUOTE_ALL)
+    #     for line in reader:
+    #         if line[0] == 'length':
+    #             continue
+    #         length = int(line[0])
+    #         scammer_chain = ast.literal_eval(line[1])
+    #         my_result.append((length, scammer_chain))
+    #
+    # print(my_result)
