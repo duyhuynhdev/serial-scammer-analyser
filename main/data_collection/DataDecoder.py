3bd9e0bab070234b51beb5438dc04df75afa35b8
Cervenjak <joshcervenjak@gmail.com>
2024-10-13 00:35:08 +1100

    add debug log to datadecoder

--- main/data_collection/DataDecoder.py
+++ main/data_collection/DataDecoder.py
@@ -225,7 +225,9 @@ class FunctionInputDecoder:
     def decode_function_input(self, function_input, signature_dict):
         data = HexBytes(function_input)
         methodId, params = data[:4], data[4:]
-        if methodId.hex() not in signature_dict.keys():
+        method_id_hex = methodId.hex()
+        print("method id hex value is={}".format(method_id_hex))
+        if method_id_hex not in signature_dict.keys():
             return False, []
         function_info = signature_dict[methodId.hex()]
         types = function_info["types"]
