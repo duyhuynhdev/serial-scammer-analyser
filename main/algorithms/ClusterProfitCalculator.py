89d5ccdbf0989717e91470e39dcc2e92eec5b300
hooniez <myeonghoon.sun.coder@gmail.com>
2024-10-12 11:43:51 +1100

    feat: Enhance Pool class with attributes x, y, z, and profit; add cached property for investing node addresses
    - Added attributes x, y, z, and profit to the Pool class.
    - Implemented a cached property for accessing investing node addresses.
    - Updated pool profit calculation to print results sorted in ascending order by profit.

--- main/algorithms/ClusterProfitCalculator.py
+++ main/algorithms/ClusterProfitCalculator.py
@@ -1,14 +1,40 @@
+import functools
 from functools import cached_property
+from collections import namedtuple
 
 from data_collection.AccountCollector import TransactionCollector
 from entity.Cluster import ClusterNode
 from entity.Node import NodeLabel
-from entity.blockchain.Address import Pool, SwapDirection, Token
-from typing import Set, List, Union
+from entity.blockchain.Address import Pool, Token
+from typing import Set, List, Union, Tuple
 
 from entity.blockchain.Transaction import NormalTransaction
 from utils import DataLoader
 from utils.Utils import get_transaction_by_hash
+
+
+def log_cluster_calculation(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print(
+            f"{'=' * 100}\n"
+            f"       Starting Calculation for Cluster: {args[1]}       \n"
+            f"{'=' * 100}"
+        )
+
+        result = func(*args, **kwargs)
+
+        print(
+            f"{'=' * 100}\n"
+            f"       Completed Calculation for Cluster: {args[1]}       \n"
+            f"{'=' * 100}"
+        )
+
+        print(f"The entire cluster profit is {result}")
+        return result
+
+    return wrapper
+
 
 class ClusterProfitCalculator:
     def __init__(self):
@@ -34,21 +60,9 @@ class ClusterProfitCalculator:
             node.address.lower()
             for node in self.cluster
             if NodeLabel.is_scammer(node)
-               # TODO: remove this line once we're confident that the given cluster does not
-               #  include contracts
-           and node.address
-           in self.dataloader.scammers_set
-        }
-
-    @cached_property
-    def wash_trader_node_addresses(self) -> Set[str]:
-        """
-        Returns a set of wash trader nodes in the cluster.
-        """
-        return {
-            node.address.lower()
-            for node in self.cluster
-            if NodeLabel.is_wash_trader(node)
+            # TODO: remove this line once we're confident that the given cluster does not
+            #  include contracts
+            and node.address in self.dataloader.scammers_set
         }
 
     @cached_property
@@ -75,6 +89,7 @@ class ClusterProfitCalculator:
         self.__dict__.pop("scammer_node_addresses", None)
         self.__dict__.pop("scammer_pools", None)
 
+    @log_cluster_calculation
     def calculate(self, cluster_name: str) -> float:
         """
         Calculates the total profit from the given cluster_name
@@ -84,27 +99,41 @@ class ClusterProfitCalculator:
         """
         self._initialise_cluster(cluster_name)
 
-        return sum(
-            self.calculate_profit_per_pool(scammer_pool) for scammer_pool in self.scammer_pools
-        )
+        ProfitPool = namedtuple("ProfitPool", ["profit", "pool"])
+
+        for scammer_pool in self.scammer_pools:
+            self.update_profit_metrics_per_pool(scammer_pool)
+
+        profits_and_pools = [
+            ProfitPool(scammer_pool.profit, scammer_pool)
+            for scammer_pool in self.scammer_pools
+        ]
+
+        profits_and_pools.sort(key=lambda x: x.profit)
+
+        for _, pool in profits_and_pools:
+            self.log_pool_calculation_results(pool)
+
+        return sum(profit_and_pool.profit for profit_and_pool in profits_and_pools)
 
     def calculate_y_per_pool(self, pool: Pool) -> float:
         """
         Calculates the total value (y) of a pool.
 
         This function evaluates the total burn value and associated fees, as well as the total
-        swap-out value and fees. Swaps are only considered if the destination address (`swap_to`)
-        is included in the set of node addresses in the cluster
+        rug-pulling withdrawal value and fees by nodes in the scam cluster.
         """
         y = 0.0
 
         burn_total, fee_total = pool.calculate_total_burn_value_and_fees()
         y += burn_total - fee_total
 
-        rug_pulling_swap_total, fee_total = (
+        rug_pulling_withdrawal_total, fee_total = (
             pool.calculate_total_divesting_value_and_fees_by_addressees(
-             self.node_addresses_in_cluster))
-        y += rug_pulling_swap_total - fee_total
+                self.node_addresses_in_cluster
+            )
+        )
+        y += rug_pulling_withdrawal_total - fee_total
 
         return y
 
@@ -125,36 +154,44 @@ class ClusterProfitCalculator:
 
         return x
 
-    def _validate_scam_token_is_created_by_cluster(self, scam_token: Token, pool: Pool) -> None:
+    def _validate_scam_token_is_created_by_cluster(
+        self, scam_token: Token, pool: Pool
+    ) -> None:
         """
         Validate that the scam token creator is in the cluster.
         """
         if scam_token.creator not in self.node_addresses_in_cluster:
-            raise ValueError(f"Scam token creator {scam_token.creator} of the scam token {scam_token.name} used in "
-                             f"the pool {pool.address} is not in the cluster's node addresses within"
-                             f" {self.cluster_name}.")
+            raise ValueError(
+                f"Scam token creator {scam_token.creator} of the scam token"
+                f" {scam_token.name} used in the pool {pool.address} is not in the "
+                f"cluster's node addresses within {self.cluster_name}."
+            )
 
-    def _validate_transaction_amount_is_zero(self, transaction: NormalTransaction, pool: Pool) -> None:
+    def _validate_transaction_amount_is_zero(
+        self, transaction: NormalTransaction, pool: Pool
+    ) -> None:
         """
         Validate that the transaction amount is zero.
         """
         if (transaction_amount := transaction.get_transaction_amount()) != 0:
             raise ValueError(
-                f"The transaction amount for the transaction hash {transaction.hash} is expected to be zero, "
-                f"but it is {transaction_amount} in pool {pool.address} within the cluster {self.cluster_name}."
+                f"The transaction amount for the transaction hash {transaction.hash} is expected "
+                f"to be zero, but it is {transaction_amount} in pool {pool.address} within the "
+                f"cluster {self.cluster_name}."
             )
 
     def calculate_token_creation_fee(self, pool: Pool) -> float:
         """
         Calculates the creation fee for a scam token in the specified pool.
 
-        This method retrieves the scam token's creator and the transaction hash used for its creation.
-        It checks if the creator is a node address in the cluster; if not, it raises an error since the creator is
-        expected to be within the set.
+        This method retrieves the scam token's creator and the transaction hash used for its
+        creation. It checks if the creator is a node address in the cluster; if not, it raises an
+        error since the creator is expected to be within the set.
 
-        If the creator is valid, the method retrieves all transactions associated with the creator's address
-        and uses the `one` function to obtain the transaction fee for the specific creation transaction,
-        which is guaranteed to be present in the list of normal transactions.
+        If the creator is valid, the method retrieves all transactions associated with the
+        creator's address and uses the `one` function to obtain the transaction fee for the
+        specific creation transaction, which is guaranteed to be present in the list of normal
+        transactions.
         """
         token_creation_fee = 0.0
 
@@ -173,27 +210,64 @@ class ClusterProfitCalculator:
 
     def calculate_z_per_pool(self, pool: Pool) -> float:
         """
-        Calculate the total 'z' value for a pool by summing disingenuous investing value and fees.
+        Calculate the total 'z' value for a pool by summing disingenuous investing value and fees
+        by nodes in the scam cluster.
         """
         z = 0.0
 
         disingenuous_investing_value_total, fee_total = (
             pool.calculate_total_investing_value_and_fees_by_addressees(
-            self.node_addresses_in_cluster))
+                self.node_addresses_in_cluster
+            )
+        )
         z += disingenuous_investing_value_total + fee_total
         return z
 
+    def get_legitimate_investor_node_addresses(self, pool: Pool) -> Set[str]:
+        return pool.investing_node_addresses - self.node_addresses_in_cluster
 
-    def calculate_profit_per_pool(self, pool: Pool) -> float:
+    def get_scam_investor_node_addresses(self, pool: Pool) -> Set[str]:
+        return pool.investing_node_addresses & self.node_addresses_in_cluster
+
+    @staticmethod
+    def calculate_profit_per_pool(x: float, y: float, z: float) -> float:
+        return y - x - z
+
+    def update_profit_metrics_per_pool(self, pool: Pool) -> None:
         """
         Calculate the true profit made in a given pool.
         """
         y = self.calculate_y_per_pool(pool)
         x = self.calculate_x_per_pool(pool)
         z = self.calculate_z_per_pool(pool)
+        profit = self.calculate_profit_per_pool(x, y, z)
 
-        return y - x - z
+        pool.x = x
+        pool.y = y
+        pool.z = z
+        pool.profit = profit
 
+    def log_pool_calculation_results(self, pool: Pool) -> None:
+        # Get legitimate and scam investor node addresses
+        legitimate_addresses = self.get_legitimate_investor_node_addresses(pool)
+        scam_addresses = self.get_scam_investor_node_addresses(pool)
+
+        # Format the addresses for logging
+        legitimate_addresses_str = (
+            "\n - ".join(legitimate_addresses) if legitimate_addresses else "None"
+        )
+        scam_addresses_str = "\n - ".join(scam_addresses) if scam_addresses else "None"
+
+        print(
+            f"Pool address: {pool.address}\n"
+            f"Value of y: {pool.y}\n"
+            f"Value of x: {pool.x}\n"
+            f"Value of z: {pool.z}\n"
+            f"Legitimate investor node addresses:\n - {legitimate_addresses_str}\n"
+            f"Scam investor node addresses:\n - {scam_addresses_str}\n"
+            f"The profit of this pool is {pool.profit}"
+            f"\n"
+        )
 
     def calculate_batch(self, cluster_names: List[str]) -> List[float]:
         """
@@ -204,6 +278,4 @@ class ClusterProfitCalculator:
 
 if __name__ == "__main__":
     calculator = ClusterProfitCalculator()
-    cluster_profit = calculator.calculate("cluster_0x19b98792e98c54f58c705cddf74316aec0999aa6")
-    print(cluster_profit)
-
+    calculator.calculate("cluster_150")
